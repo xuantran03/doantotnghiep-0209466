557359ea7bb00170c999b26f61d71bf4949816d1
xuantran0209466 <0209466@st.huce.edu.vn>
2025-05-07 20:53:02 +0700

    init commit

---
+++ db/prisma.ts
@@ -0,0 +1,79 @@
+// lib/prisma.ts
+import { PrismaClient } from '@prisma/client';
+
+// Khởi tạo Prisma client mặc định, không dùng adapter Neon
+export const prisma = new PrismaClient().$extends({
+  result: {
+    product: {
+      price: {
+        compute(product) {
+          return product.price.toString();
+        },
+      },
+      rating: {
+        compute(product) {
+          return product.rating.toString();
+        },
+      },
+    },
+    cart: {
+      itemsPrice: {
+        needs: { itemsPrice: true },
+        compute(cart) {
+          return cart.itemsPrice.toString();
+        },
+      },
+      shippingPrice: {
+        needs: { shippingPrice: true },
+        compute(cart) {
+          return cart.shippingPrice.toString();
+        },
+      },
+      taxPrice: {
+        needs: { taxPrice: true },
+        compute(cart) {
+          return cart.taxPrice.toString();
+        },
+      },
+      totalPrice: {
+        needs: { totalPrice: true },
+        compute(cart) {
+          return cart.totalPrice.toString();
+        },
+      },
+    },
+    order: {
+      itemsPrice: {
+        needs: { itemsPrice: true },
+        compute(order) {
+          return order.itemsPrice.toString();
+        },
+      },
+      shippingPrice: {
+        needs: { shippingPrice: true },
+        compute(order) {
+          return order.shippingPrice.toString();
+        },
+      },
+      taxPrice: {
+        needs: { taxPrice: true },
+        compute(order) {
+          return order.taxPrice.toString();
+        },
+      },
+      totalPrice: {
+        needs: { totalPrice: true },
+        compute(order) {
+          return order.totalPrice.toString();
+        },
+      },
+    },
+    orderItem: {
+      price: {
+        compute(item) {
+          return item.price.toString();
+        },
+      },
+    },
+  },
+});
---
+++ prisma/migrations/20241205162619_add_featured_default/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "Product" ALTER COLUMN "isFeatured" SET DEFAULT false;
